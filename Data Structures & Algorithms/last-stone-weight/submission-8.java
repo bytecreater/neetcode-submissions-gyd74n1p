e2ce936406d29240a115358e0883bd359b7e16d0
Nihal Ahemad Khan <nihalahemad2003@gmail.com>
2026-07-29 13:01:41 +0530

    Add: last-stone-weight - submission-8

---
+++ Data Structures & Algorithms/last-stone-weight/submission-8.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int stone : stones){
+            pq.offer(stone);
+        }
+        while(pq.size() > 1){
+            int a = pq.poll();
+            int b = pq.poll();
+            if(a == b){
+                continue;
+            }else{
+                pq.offer(a-b);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
