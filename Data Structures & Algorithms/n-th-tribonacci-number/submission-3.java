2f7a6f8fcb20958b444b210765905b8455df6a54
Nihal Ahemad Khan <nihalahemad2003@gmail.com>
2026-08-01 12:28:18 +0530

    Add: n-th-tribonacci-number - submission-3

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int T0 = 0;
+        int T1 = 1;
+        int T2 = 1;
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+        int ans = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        map.put(n, ans);
+
+        return ans;
+    }
+}
