230c2b4a9b83302dc66e67862596e2e6076e29f9
Nihal Ahemad Khan <nihalahemad2003@gmail.com>
2026-08-01 12:22:39 +0530

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int T0 = 0;
+        int T1 = 1;
+        int T2 = 1;
+
+        for(int i = 3; i <= n; i++){
+            int curr = T0 + T1 + T2;
+            T0 = T1;
+            T1 = T2;
+            T2 = curr;
+        }
+
+        return T2;
+    }
+}
